f193cf99951c4d0503e5bd8060cc27a16a3cd71f
Santhosh <sandyvenkatesan107@gmail.com>
2024-06-27 13:16:44 +0530

    first commit

---
+++ task_management_system/task_management_system_app/form.py
@@ -0,0 +1,12 @@
+# task_management_system_app/forms.py
+
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+
+    class Meta:
+        model = forms.Model
+        fields = ['username', 'password']
